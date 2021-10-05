0649a80e5b0826a7a95415847982f87dc580b237
PC02Z011\LacalAdmin <Yoshida.Takumi@cti.co.jp>
2021-10-05 13:18:42 +0900

    bug fix

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === 'TASK_ARCHIVED'}
+          defaultChecked={state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
+        />
       </div>
 
-      <div className="actions" onClick={event => event.stopPropagation()}>
-        {state !== 'TASK_ARCHIVED' && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
           <a onClick={() => onPinTask(id)}>
             <span className={`icon-star`} />
           </a>
@@ -28,19 +37,12 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     </div>
   );
 }
-
 Task.propTypes = {
-  /** Composition of the task */
   task: PropTypes.shape({
-    /** Id of the task */
     id: PropTypes.string.isRequired,
-    /** Title of the task */
     title: PropTypes.string.isRequired,
-    /** Current state of the task */
     state: PropTypes.string.isRequired,
   }),
-  /** Event to change the task to archived */
   onArchiveTask: PropTypes.func,
-  /** Event to change the task to pinned */
   onPinTask: PropTypes.func,
 };
